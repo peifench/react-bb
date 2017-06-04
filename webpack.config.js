4b05e49515f814d41ccd8ba7524eca294cd07963
Samer Buna <samer.buna@gmail.com>
2017-06-04 15:09:33 -0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,38 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
-  entry: './lib/renderers/domRender.js',
+  entry: {
+    vendor: ['react', 'react-dom'],
+    app: './lib/renderers/domRender.js'
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.js',
+    }),
+  ],
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
+
 
 module.exports = config;
